3391b266f8ef6686c004356366f6e68064885ee1
Rafael Avelar <rafael.avelar.makingdevs@kueski.com>
2022-04-18 17:59:29 -0500

    Add constructor to Game, create history

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    if (alculateWinner(squares) || squares[i]) {
+    if (calculateWinner(squares) || squares[i]) {
       return;
     }
     squares[i] = this.state.xIsNext ? "X" : "O";
@@ -73,6 +73,17 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array(9).fill(null),
+        },
+      ],
+      xIsNext: true,
+    };
+  }
   render() {
     return (
       <div className="game">
