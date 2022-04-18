ea30d97127b689ebaec4834e61210c994cac297b
Rafael Avelar <rafael.avelar.makingdevs@kueski.com>
2022-04-18 10:50:22 -0500

    Replace props for state in render button

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class Square extends React.Component {
   render() {
     return (
       <button className="square" onClick={() => console.log("click")}>
-        {this.props.value}
+        {this.state.value}
       </button>
     );
   }
